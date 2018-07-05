f6b727887fa9ae245d48d844ba2d422432a7980e
Guillaume de Sagazan <yazgazan@gmail.com>
2018-07-05 12:03:45 +0200

    Adding tests to cover JSON-like output

--- diff/output_test.go
+++ diff/output_test.go
@@ -1,6 +1,7 @@
 package diff
 
 import (
+	"errors"
 	"strings"
 	"testing"
 )
@@ -16,6 +17,7 @@ func TestOutput(t *testing.T) {
 				Indent:    "\t",
 				Colorized: false,
 				ShowTypes: true,
+				JSON:      false,
 			},
 			WantVal:  []string{"int", "5"},
 			WantType: []string{"int"},
@@ -25,6 +27,7 @@ func TestOutput(t *testing.T) {
 				Indent:    "\t",
 				Colorized: false,
 				ShowTypes: false,
+				JSON:      false,
 			},
 			WantVal:  []string{"5"},
 			WantType: []string{},
@@ -34,6 +37,17 @@ func TestOutput(t *testing.T) {
 				Indent:    "\t",
 				Colorized: true,
 				ShowTypes: false,
+				JSON:      false,
+			},
+			WantVal:  []string{"5"},
+			WantType: []string{},
+		},
+		{
+			Output: Output{
+				Indent:    "\t",
+				Colorized: true,
+				ShowTypes: false,
+				JSON:      true,
 			},
 			WantVal:  []string{"5"},
 			WantType: []string{},
@@ -43,6 +57,7 @@ func TestOutput(t *testing.T) {
 				Indent:    "\t",
 				Colorized: true,
 				ShowTypes: true,
+				JSON:      false,
 			},
 			WantVal:  []string{"int", "5"},
 			WantType: []string{"int"},
@@ -57,6 +72,23 @@ func TestOutput(t *testing.T) {
 		typ := test.Output.typ(5)
 		testOut(t, "Output.Type(5)", typ, test.WantType)
 	}
+}
+
+type erroringMarshaler struct{}
+
+func (erroringMarshaler) MarshalJSON() ([]byte, error) {
+	return nil, errors.New("erroringMarshaler error")
+}
+
+func TestJSONStringPanic(t *testing.T) {
+	defer func() {
+		panicV := recover()
+		if panicV == nil {
+			t.Error("Expected jsonString to panic")
+		}
+	}()
+
+	jsonString(erroringMarshaler{})
 }
 
 func testOut(t *testing.T, expr, result string, wantStrings []string) {
