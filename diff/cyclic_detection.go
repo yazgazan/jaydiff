c4a49251af4bc93907f3d9eadf4734afd9ebe548
Guillaume de Sagazan <yazgazan@gmail.com>
2018-12-06 12:53:04 +0100

    unsafe.Pointer -> uintptr: Wether the underlying value is still accessible or has been garbage collected is irrelevant to us

--- diff/cyclic_detection.go
+++ diff/cyclic_detection.go
@@ -3,7 +3,6 @@ package diff
 import (
 	"errors"
 	"reflect"
-	"unsafe"
 )
 
 // ErrCyclic is returned when one of the compared values contain circular references
@@ -12,8 +11,8 @@ var ErrCyclic = errors.New("circular references not supported")
 // visited is used to detect cyclic structures.
 // It is not safe for concurent use.
 type visited struct {
-	lhs []unsafe.Pointer
-	rhs []unsafe.Pointer
+	lhs []uintptr
+	rhs []uintptr
 }
 
 // add will try to add the value's pointers to the list. It will return an error
@@ -21,32 +20,32 @@ type visited struct {
 // visited.remove should be called whether an error occured or not.
 func (v *visited) add(lhs, rhs reflect.Value) error {
 	if canAddr(lhs) && !isEmptyMapOrSlice(lhs) {
-		if inPointers(v.lhs, unsafe.Pointer(lhs.Pointer())) {
+		if inPointers(v.lhs, lhs.Pointer()) {
 			return ErrCyclic
 		}
-		v.lhs = append(v.lhs, unsafe.Pointer(lhs.Pointer()))
+		v.lhs = append(v.lhs, lhs.Pointer())
 	}
 	if canAddr(rhs) && !isEmptyMapOrSlice(rhs) {
-		if inPointers(v.rhs, unsafe.Pointer(rhs.Pointer())) {
+		if inPointers(v.rhs, rhs.Pointer()) {
 			return ErrCyclic
 		}
-		v.rhs = append(v.rhs, unsafe.Pointer(rhs.Pointer()))
+		v.rhs = append(v.rhs, rhs.Pointer())
 	}
 
 	return nil
 }
 
 func (v *visited) remove(lhs, rhs reflect.Value) {
-	if canAddr(lhs) && isLastPointer(v.lhs, unsafe.Pointer(lhs.Pointer())) {
+	if canAddr(lhs) && isLastPointer(v.lhs, lhs.Pointer()) {
 		v.lhs = v.lhs[:len(v.lhs)-1]
 	}
 
-	if canAddr(rhs) && isLastPointer(v.rhs, unsafe.Pointer(rhs.Pointer())) {
+	if canAddr(rhs) && isLastPointer(v.rhs, rhs.Pointer()) {
 		v.rhs = v.rhs[:len(v.rhs)-1]
 	}
 }
 
-func isLastPointer(pointers []unsafe.Pointer, val unsafe.Pointer) bool {
+func isLastPointer(pointers []uintptr, val uintptr) bool {
 	if len(pointers) == 0 {
 		return false
 	}
@@ -59,7 +58,7 @@ func isEmptyMapOrSlice(v reflect.Value) bool {
 	return (v.Kind() == reflect.Slice || v.Kind() == reflect.Map) && v.Len() == 0
 }
 
-func inPointers(pointers []unsafe.Pointer, val unsafe.Pointer) bool {
+func inPointers(pointers []uintptr, val uintptr) bool {
 	for _, lhs := range pointers {
 		if lhs == val {
 			return true
